ed37461a3fddb997740ad1c370239ed28bafce49
ACER-PC <kubatbekuulu.maksat@bk.ru>
2020-04-02 19:44:28 +0600

    Homework3

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(addition( int age:25, int temperature:25);
+
+    }
+
+    public static int addition(int age, int temperature) {
+        int result = (age / temperature);
+
+        if ((age > 20 && age < 45) && (temperature > -20 && temperature < 30)) {
+            System.out.println("можно идти гулять");
+            return
+
+
+        }
+    }
+}
